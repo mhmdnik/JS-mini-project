ebe51d7159aa5b381684b7e1cf2b4e46682a1d52
mhmdnik <151462664+mhmdnik@users.noreply.github.com>
2024-08-14 18:45:05 +0330

    Add files via upload

---
+++ explain-of-slider-project/source/js/script.js
@@ -0,0 +1,30 @@
+const $ = document;
+const prev = $.querySelector(".prev");
+const next = $.querySelector(".next");
+let sliderItem = $.querySelectorAll(".slider-item");
+let index = 0;
+// console.log(sliderItem);
+
+function prevItem() {
+    index--
+    sliderItem[index + 1].classList.remove('active')
+    if (index < 0) {
+        index = sliderItem.length - 1
+    }
+    sliderItem[index].classList.add('active')
+    console.log(index);
+}
+function nextItem() {
+  
+  sliderItem[index].classList.remove('active')
+  index++;
+  if (index > sliderItem.length - 1) {
+    index = 0;
+  }
+  sliderItem[index].classList.add("active");
+  console.log(index)
+}
+
+setInterval(nextItem , 4000)
+prev.addEventListener("click", prevItem);
+next.addEventListener("click", nextItem);
