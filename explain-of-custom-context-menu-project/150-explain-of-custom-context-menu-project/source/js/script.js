4a725904d9e12ec3c28eb852f4748d7ef4de5c11
mhmdnik <151462664+mhmdnik@users.noreply.github.com>
2024-08-14 17:53:16 +0330

    Add files via upload

---
+++ explain-of-custom-context-menu-project/150-explain-of-custom-context-menu-project/source/js/script.js
@@ -0,0 +1,27 @@
+﻿let body = document.querySelector('body')
+let contextMenu = document.querySelector('#contextMenu')
+
+
+body.addEventListener('contextmenu', function(event){
+    event.preventDefault()
+    if(contextMenu.style.display === 'none'){
+        contextMenu.style.display = 'flex'
+        contextMenu.style.left = event.pageX + 'px'
+        contextMenu.style.top = event.pageY + 'px'
+    } else {
+        contextMenu.style.left = event.pageX + 'px'
+        contextMenu.style.top = event.pageY + 'px'
+    }
+})
+
+body.addEventListener('click', function () {
+    if(contextMenu.style.display !== 'none'){
+        contextMenu.style.display = 'none'
+    }
+})
+
+body.addEventListener('keydown', function (event) {
+    if(event.keyCode == 27){
+        contextMenu.style.display = 'none'
+    }
+})
